cf9d07d39b03fc5ee41ed92627a6db1227a57d72
kss2002 <kss2002@users.noreply.github.com>
2026-02-04 01:13:28 +0900

    feat: Faq and Footer add

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from './_components/Hero';
 import Imonial from './_components/Imonial';
+import Faq from './_components/Faq';
+import Footer from './_components/Footer';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Imonial />
+      <Faq />
+      <Footer />
     </>
   );
 }
