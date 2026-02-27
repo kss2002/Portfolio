d36732886e8dc4350d76bd4120b59d798fc37548
kss2002 <kss2002@users.noreply.github.com>
2026-02-28 00:04:17 +0900

    fix:title modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Devloper',
+  title: 'Frontend Developer',
   description: 'It embodies imagination in code.',
 };
 
